f8bfef08e33f3c73745136b18fe6cbf2c33eb985
Carol <carolannago2005@gmail.com>
2025-06-08 04:01:34 -0300

    adicionando arquivos

--- src/pages/Yes.jsx
+++ src/pages/Yes.jsx
@@ -1,14 +1,24 @@
 import React from 'react';
 import styles from '../styles/Yes.module.css';
-import heart from '../assets/heart.svg';
+import feliz from '../assets/happy-face.svg';
+import SpotifyPlayer from '..components/SpotifyPlayer.jsx'
 
-function No() {
+function Yes() {
   return (
     <div className={styles.yesContainer}>
-        <img src={heart}  alt="coracao-partido"/>
+        <img src={feliz}  alt="carinha-feliz"/>
 
+        <div>
+          <p>Me encontre no lugar que combinamos.</p>
+        <p>Juro que não vai se arrepender!</p>
+
+        <SpotifyPlayer/>
+
+        <p>Ouça assim que possível.</p>
+
+        </div>
     </div>
   );
 }
 
-export default No;
+export default Yes;
