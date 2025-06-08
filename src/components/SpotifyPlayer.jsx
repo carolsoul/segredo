8c0ed7ce997319c923a97ef4e3892514ae4dfa61
carolsoul <carolannago2005@gmail.com>
2025-06-08 06:04:12 -0300

    spotify component

--- src/components/SpotifyPlayer.jsx
+++ src/components/SpotifyPlayer.jsx
@@ -1,19 +1,11 @@
 import React from 'react';
 
 const SpotifyPlayer = () => {
-    return (
-        <div style={{borderRadius: '12px', overflow: 'hidden'}}>
-
-            <iframe style="border-radius:12px" src="https://open.spotify.com/embed/playlist/2F9Gt8Rd8JbH7jqp5DARP9?utm_source=generator" 
-          width="100%" 
-          height="352" 
-          frameBorder="0" 
-          allowFullScreen="" 
-          allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" 
-          loading="lazy"></iframe>
-
-        </div>
-    );
+  return (
+    <div style={{ width: '370px', maxWidth: '600px', margin: '0 auto' }}>
+      <iframe style={{ borderRadius: '12px' }} src="https://open.spotify.com/embed/playlist/2F9Gt8Rd8JbH7jqp5DARP9?utm_source=generator" width="100%" height="160" frameBorder="0" allowFullScreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+    </div>
+  );
 };
 
 export default SpotifyPlayer;
